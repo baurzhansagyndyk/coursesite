8503e04e5bd82d5bd49e0c60596022c535a73a24
Bauyrjan <baurzhan_kairatylu.00@mail.ru>
2021-01-17 14:58:32 +0600

    second commit

--- student/serializers.py
+++ student/serializers.py
@@ -3,9 +3,10 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=student.objects.all())
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = User
         fields = ['id', 'username', 'snippets']
+# wpemr;mkwc
